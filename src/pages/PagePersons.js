7a77be9dc2348fe542b715bf020d8bf24b018a2c
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-19 11:41:45 +0100

    search state works

--- src/pages/PagePersons.js
+++ src/pages/PagePersons.js
@@ -1,11 +1,27 @@
+import { useState } from 'react';
 import persons from '../data/persons.json';
 
 const PagePersons = () => {
+	const [searchText, setSearchText] = useState('');
+
+	const displaySearchResults = (e) => {
+		const searchText = e.target.value;
+		setSearchText(searchText);
+
+		// if (searchText === '') {
+		// 	setItemsByIdCode('newestFirst');
+		// } else {
+		// 	const items = getItems({ idCode: '', searchText, id: 0 });
+		// 	setHowtos([...items]);
+		// }
+	}
+
 	return (
 		<div className="pagePersons">
+			[{searchText}]
 			{persons.length} Persons
 			<div className="searchArea">
-				<input type="text" />
+				<input type="text" onChange={displaySearchResults} />
 			</div>
 			<div className="personsArea">
 				{persons.map((p, i) => {
