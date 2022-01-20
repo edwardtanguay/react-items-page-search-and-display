e848a175ce11ee80e428177a46f4d79fcba3fc78
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-20 01:30:19 +0100

    home half finished

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -1,7 +1,16 @@
 const PageHome = () => {
 	return (
 		<div className="pageHome">
-			This is the main page.
+			<h1>React Item Page Demonstration</h1>
+			<ul>
+				<li>This site demonstrates how to display multiple items on a page with <strong>search feature</strong> and a <strong>click-to-see-more-details feature</strong>.</li>
+				<li>Note that the <strong>URL</strong> and the <strong>tab title</strong> change appropriately.</li>
+				<li>Note also that there are examples of two data sources: <strong>a local JSON file</strong> and <strong>APIs</strong>.</li>
+			</ul>
+
+			<h2>Page: Employees</h2>
+			<h2>Page: Customers</h2>
+			<h2>Page: Suppliers</h2>
 		</div>
 	)
 }
