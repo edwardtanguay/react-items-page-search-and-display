8c40749d3988734babaff00523034d84fdfbaf7e
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-29 23:36:08 +0100

    page header in

--- src/pages/PageCustomers.js
+++ src/pages/PageCustomers.js
@@ -3,7 +3,7 @@ import { useState, useRef, useEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
 import '../styles/pageItems.scss';
 import '../styles/pageCustomers.scss';
-import { FaSpinner } from 'react-icons/fa';
+import { ItemPageHeader } from '../components/ItemPageHeader';
 
 const pageConfig = {
 	itemSingularTitle: 'Customer',
@@ -31,7 +31,7 @@ const PageCustomers = ({ pageLoader, hocDisplaySearchResults, hocShowAllItems, h
 	});
 
 	useEffect(async () => {
-		pageLoader(pageConfig,setSearchText, setInitialItems, setFilteredItems, setFilteredItem, isSmartphone, inputSearchText);
+		pageLoader(pageConfig, setSearchText, setInitialItems, setFilteredItems, setFilteredItem, isSmartphone, inputSearchText);
 	}, []);
 
 	const displaySearchResults = (e) => {
@@ -42,7 +42,7 @@ const PageCustomers = ({ pageLoader, hocDisplaySearchResults, hocShowAllItems, h
 		hocShowSingleItem(pageConfig, item, setFilteredItems, setFilteredItem);
 	}
 
-	
+
 	const showAllItems = () => {
 		hocShowAllItems(pageConfig, setInitialItems, initialItems, setFilteredItems, setFilteredItem, setSearchText, isSmartphone, inputSearchText);
 	}
@@ -50,35 +50,7 @@ const PageCustomers = ({ pageLoader, hocDisplaySearchResults, hocShowAllItems, h
 	return (
 		<div className="pageItems pageCustomers">
 
-			<div className="totalHeader">
-				{filteredItems.length > 1 && filteredItems.length < initialItems.length && (
-					<div>
-						{filteredItems.length} of <span className="allItemsLink" onClick={showAllItems}>{initialItems.length} {pageConfig.itemPluralTitle}</span>
-					</div>
-				)}
-
-				{filteredItems.length === 1 && (
-					<div>
-						1 of <span className="allItemsLink" onClick={showAllItems}>{initialItems.length} {pageConfig.itemPluralTitle}</span>
-					</div>
-				)}
-
-				{filteredItems.length === initialItems.length && filteredItems.length !== 0 && (
-					<div>
-						<div>{initialItems.length} {pageConfig.itemPluralTitle}</div>
-					</div>
-				)}
-
-				{filteredItems.length === 0 && (
-					<div className="pageLoadingArea">
-						<div>Loading {pageConfig.itemPluralTitle.toLowerCase()}... <FaSpinner className="spinner" /> </div>
-					</div>
-				)}
-			</div>
-
-			<div className="searchArea">
-				<input type="text" ref={inputSearchText} placeholder="SEARCH" value={searchText} onFocus={displaySearchResults} onChange={displaySearchResults} />
-			</div>
+			<ItemPageHeader filteredItems={filteredItems} initialItems={initialItems} showAllItems={showAllItems} pageConfig={pageConfig} inputSearchText={inputSearchText} searchText={searchText} displaySearchResults={displaySearchResults} />
 
 			{/* MULTIPLE ITEMS */}
 			{filteredItems.length > 1 && (
